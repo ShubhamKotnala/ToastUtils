5eba49401c93467a68c195bd529f95a702e881d6
shubhamKotnala <shubham.kotnala@unthinkable.co>
2021-10-06 12:34:01 +0530

    Lib added

---
+++ toaster/src/main/java/com/example/toaster/ToasterUtils.kt
@@ -0,0 +1,18 @@
+package com.example.toaster
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
+class ToasterUtils {
+
+     companion object {
+
+         fun showRedToast(context: Context, string: String){
+             var toast : Toast = Toast.makeText(context,string, Toast.LENGTH_SHORT)
+             val view: View? = toast.view
+             view?.setBackgroundColor(context.resources.getColor(R.color.red))
+             toast.show()
+         }
+     }
+}
